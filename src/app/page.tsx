07146c15a117dc60a724946d3555e545acde0c46
alternativeprogrammer <rpriegobautista@tec.mx>
2024-10-01 17:02:23 -0600

    Refactor authentication handling and remove unused code

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import AuthScreen from "../app/login-register-screen"
-
-export default function AuthPage() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <AuthScreen />
-    </div>
-  )
-}
